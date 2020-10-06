39e05fa2b0c566783c39c281993ede4524536137
Anthony <a.dottesi@hotmail.fr>
2020-10-06 14:21:03 +0200

    nom des classes dans l'admin

--- classementLivre/models.py
+++ classementLivre/models.py
@@ -8,8 +8,8 @@ from django.contrib import admin
 
 
 class Utilisateur(models.Model):
-
     nom = models.CharField(max_length=50)
+    slug = models.SlugField()
     prenom = models.CharField(max_length=50)
     date_inscription = models.DateTimeField("date d'inscription ")
     auteur_favori = models.ForeignKey(
@@ -20,29 +20,44 @@ class Utilisateur(models.Model):
         "listeDeLecture", on_delete=models.DO_NOTHING)
     # nb_de_livre = models.IntegerField(default=0) une fonctio peut être. ???
 
+    def __str__(self):
+        return self.nom
 
+
+# table détaillant les livres lus par l'utilisateur
 class ListeDeLecture(models.Model):
-
     livre = models.ForeignKey(
         "livre", on_delete=models.DO_NOTHING)
     date_creation = models.DateTimeField(" date de création ")
 
 
+# table des livres contenant le nom et le nombre de mots liés aux tables d'auteurs et de sagas
 class Livre(models.Model):
     titre = models.CharField(max_length=200)
     saga = models.ForeignKey(
-        "saga", on_delete=models.DO_NOTHING)
+        "saga", on_delete=models.DO_NOTHING, default="livre unique")
     auteur = models.ForeignKey(
         "auteur", on_delete=models.DO_NOTHING)
     nb_de_mots = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.titreexit
 
+
+# table présentant les auteurs, prénom, nom et age, voire lien biblio??
 class Auteur(models.Model):
     prenom = models.CharField(max_length=200)
     nom = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.nom
 
+
+# table présentant les noms des sagas et le nombre de tome les composant
 class Saga(models.Model):
     nom_saga = models.CharField(max_length=200)
     nb_de_tome = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nom_saga
--- classementLivre/views.py
+++ classementLivre/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Utilisateur, Auteur, Livre, Saga
+
 # Create your views here.
 
 app_name = "classementLivre"
@@ -9,3 +11,8 @@ app_name = "classementLivre"
 def accueil(request):
     return render(request, "classementLivre/accueil.html")
     # return HttpResponse("Bienvenue sur le site du lecteur du dimanche.")
+
+
+def profile(request):
+    livres = Livre.objects.all()
+    return render(request, "classementLivre/profile.html", {"livres": livres})
