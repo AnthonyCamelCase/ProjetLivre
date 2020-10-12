7c6f23a49a017cfc6e4dc440a26cb56851a996eb
Anthony <a.dottesi@hotmail.fr>
2020-10-12 13:58:08 +0200

    lien static

--- classementLivre/urls.py
+++ classementLivre/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
-    path('', views.accueil, name='accueil'),
-    path('profile', views.profile, name="profile"),
-    path('livre/<str:titre_livre>/', views.livre, name="livre")
+    url(r'^$', views.accueil, name='accueil'),
+    url(r'^profile/$', views.profile, name="profile"),
+    url(r'^livre/<str:titre_livre>/$', views.livre, name="livre")
 ]
